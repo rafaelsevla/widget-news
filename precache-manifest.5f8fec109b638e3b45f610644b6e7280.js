58f3ed2e24edaff1e9124f90b619631abdf7edb6
Rafael Costa <rafaelsevla@gmail.com>
2019-12-25 01:07:12 -0300

    Updates

---
+++ precache-manifest.5f8fec109b638e3b45f610644b6e7280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee55ce4c2ada601091d67d2419e5e9f",
+    "url": "/widget-news/index.html"
+  },
+  {
+    "revision": "35fd6793f4c6f5b38513",
+    "url": "/widget-news/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "b5eef95786917dde762b",
+    "url": "/widget-news/static/css/main.675e4bf3.chunk.css"
+  },
+  {
+    "revision": "35fd6793f4c6f5b38513",
+    "url": "/widget-news/static/js/2.84031586.chunk.js"
+  },
+  {
+    "revision": "b5eef95786917dde762b",
+    "url": "/widget-news/static/js/main.d18eb8df.chunk.js"
+  },
+  {
+    "revision": "07adea7eaf82ca1bec3a",
+    "url": "/widget-news/static/js/runtime~main.77df6ffc.js"
+  }
+]);
